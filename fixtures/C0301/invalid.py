63f64a3ccf57797491ee92d1ad6eea77515a9448
Vladimir <dopustimvladimir@gmail.com>
2022-01-08 22:59:11 +0600

    Add fixtures for CONVENTION status

---
+++ fixtures/C0301/invalid.py
@@ -0,0 +1,2 @@
+
+notes = [{"title": f"Lorem ipsum {x}", "content": "Lorem ipsum dolor sit amet"} for x in range(1, 5)]
---
+++ fixtures/C0301/valid.py
@@ -0,0 +1,5 @@
+
+notes = [{
+    "title": f"Lorem ipsum {x}",
+    "content": "Lorem ipsum dolor sit amet"
+} for x in range(1, 5)]

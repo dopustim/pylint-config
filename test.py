71008f3b58f8ab1a954978e627b85ef05e67786d
Vladimir <dopustimvladimir@gmail.com>
2022-01-20 19:31:37 +0600

    Refactor tests

--- test.py
+++ test.py
@@ -1,17 +1,17 @@
 
-from subprocess import run, PIPE, STDOUT
-from json import loads
+import subprocess
+import json
 
 def test_valid_fixtures():
-    cmd = f"python -m pylint -f json fixtures/*/valid.py"
-    res = run(cmd, stdout=PIPE, stderr=PIPE, shell=True)
+    res = subprocess.run("pylint -f json fixtures/*/valid.py",
+        stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, check=False)
     assert res.returncode == 0
 
 def test_invalid_fixtures():
-    cmd = f"pylint -f json fixtures/*/invalid.py"
-    res = run(cmd, stdout=PIPE, stderr=PIPE, shell=True)
+    res = subprocess.run("pylint -f json fixtures/*/invalid.py",
+        stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, check=False)
     assert res.returncode != 0
-    for it in loads(res.stdout):
+    for it in json.loads(res.stdout):
         assert it["message-id"] in it["path"]
 
 if __name__ == "__main__":
